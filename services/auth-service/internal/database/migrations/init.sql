e5ae60c4d49aa731e834e577ccda0570932af7fe
pwn922 <mauricio41533@gmail.com>
2024-09-30 19:25:13 -0800

    build: migration init

--- services/auth-service/internal/database/migrations/init.sql
+++ services/auth-service/internal/database/migrations/init.sql
@@ -1,1 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE DATABASE auth_service;
+
+\c auth_service;
+
+CREATE TABLE IF NOT EXISTS roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    role_name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT
+);
+
+INSERT INTO roles (role_name, description) VALUES
+('student', 'Role for students'),
+('admin', 'Role for administrators'),
+('instructor', 'Role for instructors')
+ON CONFLICT (role_name) DO NOTHING;
