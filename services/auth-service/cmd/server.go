b21c2a9b4e6047759e7abc7d84cbf68981168d9f
pwn922 <mauricio41533@gmail.com>
2024-09-30 19:26:28 -0800

    upd: server.go

--- services/auth-service/cmd/server.go
+++ services/auth-service/cmd/server.go
@@ -7,9 +7,16 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/mux"
 	"github.com/pwn922/auth-service/graph"
+	"github.com/pwn922/auth-service/graph/middlewares"
 	"github.com/pwn922/auth-service/internal/config"
 	"github.com/pwn922/auth-service/internal/database"
+	"github.com/pwn922/auth-service/internal/handlers"
+	"github.com/pwn922/auth-service/internal/repositories"
+	"github.com/pwn922/auth-service/internal/services/auth"
+	"github.com/pwn922/auth-service/internal/services/jwt"
+	"github.com/pwn922/auth-service/internal/services/user"
 )
 
 const defaultPort = "8080"
@@ -26,12 +33,26 @@ func main() {
 	}
 
 	db := database.GetDB()
-	resolver := graph.NewResolver(db)
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
+	userRepository := repositories.NewUserRepository(db)
+	roleRepository := repositories.NewRoleRepository(db)
+	userService := user.NewUserService(userRepository, roleRepository)
+	jwtService := jwt.NewJWTService()
+	authService := auth.NewAuthService(userService, jwtService)
+	resolver := graph.NewResolver(authService)
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
+		Resolvers:  resolver,
+	}))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	router := mux.NewRouter()
+	authHandlers := handlers.NewAuthHandlers(authService)
+    router.HandleFunc("/login", authHandlers.LoginHandler)
+	router.HandleFunc("/register", authHandlers.RegisterHandler)
+	
+	authMiddleware := middlewares.NewAuthMiddleware(jwtService)
+	router.Handle("/query", authMiddleware.Authenticate(srv))
+    router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	log.Fatal(http.ListenAndServe(":"+defaultPort, router))
 }
